6cf36ed807cc107a500c34e9781a7a853ba04b3f
Malik Halabi <malik.halabi@students.mq.edu.au>
2025-03-05 16:15:04 +1100

    Few updates
    WORKING ON SPRITES

---
+++ src/main/Player.java
@@ -0,0 +1,5 @@
+package main;
+import java.awt.Graphics2D;
+public class Player {
+    
+}
---
+++ src/sprites/snakeplay.java
@@ -0,0 +1,20 @@
+package sprites;
+
+import main.Game; 
+import main.Input;
+
+public class snakeplay extends apple {
+
+    Game game;
+    Input input;
+    public snakeplay(Game game, Input input) {
+        this.game = game;
+        this.input = input;
+    }
+    public void setDef(){
+        int x = 150;
+        int y = 100;
+        int speed = 5;
+    }
+    
+}
